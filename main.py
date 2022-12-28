c6aa5c3bdb50087ede0fc53bb53cf17a565ada67
Vaishnavi Kulkarni <75296347+vaishnavik0@users.noreply.github.com>
2022-12-28 19:02:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()  # understand our voice
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    # inside try block we will use microphone
+    try:
+        with sr.Microphone() as source:
+            print('Listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M:%S %p')
+        print(time)
+        talk('Hey Vaishnavi Current time is ' + time)
+
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+        print(pyjokes)
+
+    else:
+        talk('please say the command again.')
+
+
+while True:
+    run_alexa()
+
+run_alexa()
